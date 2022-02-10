844a5c300ed19ac717d8858f6983cd34646d873f
jtrongkhoa93 <khoado2011@gmail.com>
2022-02-10 11:11:36 +0700

    publish

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import colorama
+import os
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = "static"
+
+@app.route("/", methods = ['POST', 'GET'])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        image_file = request.files['image']
+        path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename)
+        image_file.save(path_to_save)
+        return "Image uploaded"
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port=9999, debug=True)
