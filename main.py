3d7d0d4727a3ff694cac4ebdf77b74fdee97dcb0
jtrongkhoa93 <khoado2011@gmail.com>
2022-02-10 21:33:29 +0700

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
 import colorama
@@ -12,11 +14,14 @@ def home():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        image_file = request.files['image']
-        path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image_file.filename)
-        image_file.save(path_to_save)
-        prediction_img = "/darknet/predictions.jpg"
-        return render_template("index.html", user_image = path_to_save)
+        image_file_name = request.files['image'].filename
+        path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image_file_name)
+        config = "cfg/yolov4.cfg yolov4.weights " + path_to_save
+        subprocess.call("./darknet detect -config " + config)
+
+        os.replace("predictions.jpg", path_to_save)
+
+        return render_template("index.html", user_image=path_to_save)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=9999, debug=True)
+    app.run(host='0.0.0.0', port=9999, debug=True)
