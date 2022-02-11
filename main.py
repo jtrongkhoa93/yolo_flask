0bd2dad6086691c9473e16f7949b9717b912ce7d
jtrongkhoa93 <khoado2011@gmail.com>
2022-02-11 11:57:29 +0700

    commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import subprocess
+from io import BytesIO
 
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
 import colorama
 import os
+import logging
 
 app = Flask(__name__)
+
+app.logger.disabled = True
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 app.config['UPLOAD_FOLDER'] = "static"
 
@@ -15,14 +21,15 @@ def home():
         return render_template("index.html")
     else:
         image_file = request.files['image']
-        # path_to_save = os.path.join(app.config['UPLOAD_FOLDER'], image_file_name)
+
         path_to_save = os.path.join("data", image_file.filename)
         image_file.save(path_to_save)
+        image_size = os.stat(path_to_save).st_size
+
         config = "cfg/yolov4.cfg yolov4.weights " + path_to_save
-        # subprocess.call("../darknet detect -config " + config)
+
         subprocess_out = subprocess.Popen("./darknet detect " + config, shell=True, stdout=subprocess.PIPE)
         subprocess_return = subprocess_out.stdout.read()
-        print(subprocess_return)
         path_to_display = os.path.join("../static", image_file.filename)
 
         subprocess_out.wait()
@@ -30,8 +37,22 @@ def home():
         os.replace("predictions.jpg", os.path.join("static", image_file.filename))
 
         count_of_person = str(subprocess_return).count("\\nperson")
+        app_logger(count_of_person, image_file.filename, image_size)
 
         return render_template("index.html", user_image=path_to_display, exe_output=count_of_person)
+
+
+def app_logger(person_count, image_name, image_size):
+    logging.basicConfig(filename="app_log",
+                        filemode='a',
+                        format='%(asctime)s %(name)s %(levelname)s %(message)s',
+                        datefmt="%Y-%m-%d %H:%M:%S",
+                        level=logging.INFO)
+
+    logger = logging.getLogger("yolo_app")
+
+    logger.info("%i - %s - %.2f" % (person_count, image_name, image_size))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9999, debug=True)
